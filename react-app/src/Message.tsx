c100a4530ccf1ae24068646dd069cb9856cc9456
Anand <tanishanand548@gmail.com>
2025-01-12 13:35:33 +0530

    React + Vite + Ts

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message() {
+    //JSX: Javascript XML
+    const name = ''
+    if (name)
+        return <h1>Hello {name}</h1>
+    else 
+        return <h1>Hello World</h1>
+}
+
+export default Message;
